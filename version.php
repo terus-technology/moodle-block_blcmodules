06a0b99b89de38b6a4f4ee4592808415be6a2bbe
teruselearning <49330603+teruselearning@users.noreply.github.com>
2022-05-03 17:16:54 +0700

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022050301;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022041900;        // Requires this Moodle version
+$plugin->requires  = 2016052300;        // Requires this Moodle version
 $plugin->component = 'block_blc_modules'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.0';
+$plugin->release = '4.0.1';
